69c3de59dd2f4e53705e6e699254497cd61d833d
wayne05IM <wayne514710@gmail.com>
2022-11-29 22:37:15 +0800

    hack2

--- hack2/frontend/src/components/filter.js
+++ hack2/frontend/src/components/filter.js
@@ -6,83 +6,118 @@
   Copyright     [ 2022 11 ]
 ****************************************************************************/
 
-import React from 'react'
-import '../css/navigationBar.css'
+import React from "react";
+import "../css/navigationBar.css";
 
-const Filter = ({ priceFilter, setPriceFilter, mealFilter, setMealFilter, typeFilter, setTypeFilter, setDisplay }) => {
-    const PriceTag = ["$", "$$", "$$$"]
-    const MealTag = ["Breakfast", "Lunch", "Dinner"]
-    const TypeTag = ["Chinese", "American", "Italian", "Japanese", "Korean", "Thai"]
-    const getTagString = () => {
-        const getSingleCategoryTag = (tag, filter, result) => {
-            for(let i=0; i<tag.length; i++){
-                if(filter.includes(tag[i])){
-                    if(result !== "") result += ", "
-                    result += tag[i]
-                }
-            }
-            return result
+const Filter = ({
+  priceFilter,
+  setPriceFilter,
+  mealFilter,
+  setMealFilter,
+  typeFilter,
+  setTypeFilter,
+  setDisplay,
+}) => {
+  const PriceTag = ["$", "$$", "$$$"];
+  const MealTag = ["Breakfast", "Lunch", "Dinner"];
+  const TypeTag = [
+    "Chinese",
+    "American",
+    "Italian",
+    "Japanese",
+    "Korean",
+    "Thai",
+  ];
+  const getTagString = () => {
+    const getSingleCategoryTag = (tag, filter, result) => {
+      for (let i = 0; i < tag.length; i++) {
+        if (filter.includes(tag[i])) {
+          if (result !== "") result += ", ";
+          result += tag[i];
         }
-        let result = ""
+      }
+      return result;
+    };
+    let result = "";
 
-        result = getSingleCategoryTag(PriceTag, priceFilter, result)
-        result = getSingleCategoryTag(MealTag, mealFilter, result)
-        result = getSingleCategoryTag(TypeTag, typeFilter, result)
-        
-        return result
+    result = getSingleCategoryTag(PriceTag, priceFilter, result);
+    result = getSingleCategoryTag(MealTag, mealFilter, result);
+    result = getSingleCategoryTag(TypeTag, typeFilter, result);
+
+    return result;
+  };
+
+  const modifyFilter = (key, filter) => {
+    // TODO Part II-1: change filter state on clicking the pertaining checkboxes
+    if (filter.indexOf(key) != -1) {
+      const place = filter.indexOf(key);
+      filter.splice(place, 1);
+    } else {
+      filter.push(key);
     }
+    return filter;
+  };
 
-    const modifyFilter = (key, filter) => {
-        // TODO Part II-1: change filter state on clicking the pertaining checkboxes
-        
-        return filter
-    }
+  const modifyPriceFilter = (key) => {
+    priceFilter = modifyFilter(key, priceFilter);
+    setPriceFilter(priceFilter);
+    setDisplay(getTagString());
+  };
 
-    const modifyPriceFilter = (key) => {
-        priceFilter = modifyFilter(key, priceFilter)
-        setPriceFilter(priceFilter)
-        setDisplay(getTagString())
-    }
+  const modifyMealFilter = (key) => {
+    mealFilter = modifyFilter(key, mealFilter);
+    setMealFilter(mealFilter);
+    setDisplay(getTagString());
+  };
 
-    const modifyMealFilter = (key) => {
-        mealFilter = modifyFilter(key, mealFilter)
-        setMealFilter(mealFilter)
-        setDisplay(getTagString())
-    }
+  const modifyTypeFilter = (key) => {
+    typeFilter = modifyFilter(key, typeFilter);
+    setTypeFilter(typeFilter);
+    setDisplay(getTagString());
+  };
 
-    const modifyTypeFilter = (key) => {
-        typeFilter = modifyFilter(key, typeFilter)
-        setTypeFilter(typeFilter)
-        setDisplay(getTagString())
-    }
-
-    return (
-        <div className='filterDetail'>
-            <div className='category'>
-                {PriceTag.map((key) => (
-                    <div className='inputBlock' key={key} id={key}>
-                        <input type="checkbox" value={key} defaultChecked={priceFilter.includes(key)} onChange={e => modifyPriceFilter(e.target.value)} />
-                        <label htmlFor={key}> {key} </label>
-                    </div>
-                ))}
-            </div>
-            <div className='category'>
-                {MealTag.map((key) => (
-                    <div className='inputBlock' key={key} id={key}>
-                        <input type="checkbox" value={key} defaultChecked={mealFilter.includes(key)} onChange={e => modifyMealFilter(e.target.value)} />
-                        <label htmlFor={key}> {key} </label>
-                    </div>
-                ))}
-            </div>
-            <div className='category'>
-                {TypeTag.map((key) => (
-                    <div className='inputBlock' key={key} id={key}>
-                        <input type="checkbox" value={key} defaultChecked={typeFilter.includes(key)} onChange={e => modifyTypeFilter(e.target.value)} />
-                        <label htmlFor={key}> {key} </label>
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
-}
-export default Filter
+  return (
+    <div className="filterDetail">
+      <div className="category">
+        {PriceTag.map((key) => (
+          <div className="inputBlock" key={key} id={key}>
+            <input
+              type="checkbox"
+              value={key}
+              defaultChecked={priceFilter.includes(key)}
+              onChange={(e) => modifyPriceFilter(e.target.value)}
+            />
+            <label htmlFor={key}> {key} </label>
+          </div>
+        ))}
+      </div>
+      <div className="category">
+        {MealTag.map((key) => (
+          <div className="inputBlock" key={key} id={key}>
+            <input
+              type="checkbox"
+              value={key}
+              defaultChecked={mealFilter.includes(key)}
+              onChange={(e) => modifyMealFilter(e.target.value)}
+            />
+            <label htmlFor={key}> {key} </label>
+          </div>
+        ))}
+      </div>
+      <div className="category">
+        {TypeTag.map((key) => (
+          <div className="inputBlock" key={key} id={key}>
+            <input
+              type="checkbox"
+              value={key}
+              defaultChecked={typeFilter.includes(key)}
+              onChange={(e) => modifyTypeFilter(e.target.value)}
+            />
+            <label htmlFor={key}> {key} </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Filter;
