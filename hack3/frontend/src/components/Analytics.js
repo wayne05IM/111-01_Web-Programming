32342ba26f1751386879c72041a62206161ac6ca
wayne05IM <wayne514710@gmail.com>
2022-12-20 11:32:59 +0800

    hack3

--- hack3/frontend/src/components/Analytics.js
+++ hack3/frontend/src/components/Analytics.js
@@ -8,9 +8,9 @@ import Category from './Category';
 function Analytics() {
   // TODO 2.2 Use the useQuery hook to get items from backend
   const {
-    loading, error, data: itemsData, subscribeToMore,
+    loading, error, data: { items },
   } = useQuery(GET_ITEMS_QUERY);
-  const { items } = itemsData;
+  // const { items } = itemsData;
   // TODO 2.2 End
 
 
