0513f879eb547ad1137dca92ed006f205dd9b834
wayne05IM <wayne514710@gmail.com>
2022-12-25 17:04:24 +0800

    deploy

--- hw6/backend/src/server.js
+++ hw6/backend/src/server.js
@@ -1,15 +1,27 @@
 import express from "express";
-import db from './db';
+import db from "./db";
 import cors from "cors";
-import routes from './routes';
+import routes from "./routes";
 
 const app = express();
-const port = process.env.PORT || 4000;
 
 db.connect();
 
+if (process.env.NODE_ENV === "development") {
+	app.use(cors());
+}
+
 app.use(cors());
 app.use(express.json());
-app.use('/', routes);
+app.use("/", routes);
 
+if (process.env.NODE_ENV === "production") {
+  const __dirname = path.resolve();
+  app.use(express.static(path.join(__dirname, "../frontend", "build")));
+  app.get("/*", function (req, res) {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+}
+
+const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
